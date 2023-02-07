8816b5af05cb92baf48cb1e94891e569c209c756
Saqib Suleman <saqibs3291@gmail.com>
2023-02-07 21:03:25 +0500

    added overlay card

--- pos-dribble-design/src/components/Main/MainContent/Articles/ArticleCard.js
+++ pos-dribble-design/src/components/Main/MainContent/Articles/ArticleCard.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./ArticleCard.module.css";
 
 function ArticleCard({ article }) {
+  const [mouseEnter, setMouseEnter] = useState(false);
+
+  const mouseEnterHandler = () => {
+    setMouseEnter(true);
+  };
+
+  // const mouseLeaveHandler = () => {
+  //   setMouseEnter(false);
+  // };
+  //      onMouseLeave={mouseLeaveHandler}
+
   return (
-    <div className={classes.Card}>
-      <article className={classes.upperBody}>
-        <img src={article.imgUrl} />
-      </article>
-      <span className={classes.lowerBody}>{article.name}</span>
-    </div>
+    <>
+      <div className={classes.Card} onMouseEnter={mouseEnterHandler}>
+        <article className={classes.upperBody}>
+          <img src={article.imgUrl} />
+        </article>
+        <span className={classes.lowerBody}>{article.name}</span>
+        {mouseEnter ? (
+          <div className={classes.on_enter}>
+            <span>{article.price}</span>
+            <button>Add to cart</button>
+          </div>
+        ) : null}
+      </div>
+    </>
   );
 }
 
