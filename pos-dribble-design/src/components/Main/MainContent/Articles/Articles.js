01dfe7f349d87075d03cf1bac3fce9c34d693024
Saqib Suleman <saqibs3291@gmail.com>
2023-02-07 18:11:07 +0500

    updated article rendering

--- pos-dribble-design/src/components/Main/MainContent/Articles/Articles.js
+++ pos-dribble-design/src/components/Main/MainContent/Articles/Articles.js
@@ -1,16 +1,15 @@
 import React from "react";
 import CardsWrapper from "../../../UI/CardsWrapper";
 import ArticleCard from "./ArticleCard";
-import { VANS_ARTICLES_DATA } from "../../../../seeds/INVENTORY";
 
-function VansArticles() {
+function Articles({ articles }) {
   return (
     <CardsWrapper>
-      {VANS_ARTICLES_DATA.map((article) => (
+      {articles.map((article) => (
         <ArticleCard key={article.id} article={article} />
       ))}
     </CardsWrapper>
   );
 }
 
-export default VansArticles;
+export default Articles;
--- pos-dribble-design/src/components/Main/MainContent/MainContent.js
+++ pos-dribble-design/src/components/Main/MainContent/MainContent.js
@@ -2,23 +2,41 @@ import React from "react";
 import Brands from "./Brands/Brands";
 import classes from "./MainContent.module.css";
 import { Route, Routes } from "react-router-dom";
-import NikeArticles from "./Articles/NikeArticles";
-import AdidasArticles from "./Articles/AdidasArticles";
-import ReebokArticles from "./Articles/ReebokArticles";
-import PumaArticles from "./Articles/PumaArticles";
-import VansArticles from "./Articles/VansArticles";
+import Articles from "./Articles/Articles";
+import {
+  NIKE_ARTICLES_DATA,
+  ADIDAS_ARTICLES_DATA,
+  REEBOK_ARTICLES_DATA,
+  PUMA_ARTICLES_DATA,
+  VANS_ARTICLES_DATA,
+} from "../../../seeds/INVENTORY";
 
 function MainContent() {
   return (
     <div className={classes.mainContent}>
       <Brands />
       <Routes>
-        <Route path="/nike" element={<NikeArticles />} />
-        <Route path="/adidas" element={<AdidasArticles />} />
-        <Route path="/reebok" element={<ReebokArticles />} />
-        <Route path="/puma" element={<PumaArticles />} />
-        <Route path="/vans" element={<VansArticles />} />
-        <Route path="/" element={<NikeArticles />} />
+        <Route
+          path="/nike"
+          element={<Articles articles={NIKE_ARTICLES_DATA} />}
+        />
+        <Route
+          path="/adidas"
+          element={<Articles articles={ADIDAS_ARTICLES_DATA} />}
+        />
+        <Route
+          path="/reebok"
+          element={<Articles articles={REEBOK_ARTICLES_DATA} />}
+        />
+        <Route
+          path="/puma"
+          element={<Articles articles={PUMA_ARTICLES_DATA} />}
+        />
+        <Route
+          path="/vans"
+          element={<Articles articles={VANS_ARTICLES_DATA} />}
+        />
+        <Route path="/" element={<Articles articles={NIKE_ARTICLES_DATA} />} />
       </Routes>
     </div>
   );
