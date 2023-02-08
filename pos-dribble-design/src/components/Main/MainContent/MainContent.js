c192cd1a99abc38596d0357bca12707982cc5275
Saqib Suleman <saqibs3291@gmail.com>
2023-02-08 20:06:50 +0500

    completed search logic and rethought routing

--- pos-dribble-design/src/components/Main/MainContent/MainContent.js
+++ pos-dribble-design/src/components/Main/MainContent/MainContent.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Brands from "./Brands/Brands";
 import classes from "./MainContent.module.css";
 import { Route, Routes } from "react-router-dom";
@@ -12,38 +12,36 @@ import {
 } from "../../../seeds/INVENTORY";
 
 function MainContent({ searchTerm }) {
-  const filtered_NIKE_ARTICLES_DATA = NIKE_ARTICLES_DATA.filter((article) =>
-    article.name.toLowerCase().includes(searchTerm)
-  );
+  const filterBySearchTerm = (articles, term = searchTerm) => {
+    return articles.filter((article) =>
+      article.name.toLowerCase().includes(term)
+    );
+  };
+
+  const filtered_NIKE_ARTICLES_DATA = filterBySearchTerm(NIKE_ARTICLES_DATA);
+  const filtered_ADIDAS_ARTICLES_DATA =
+    filterBySearchTerm(ADIDAS_ARTICLES_DATA);
+  const filtered_REEBOK_ARTICLES_DATA =
+    filterBySearchTerm(REEBOK_ARTICLES_DATA);
+  const filtered_PUMA_ARTICLES_DATA = filterBySearchTerm(PUMA_ARTICLES_DATA);
+  const filtered_VANS_ARTICLES_DATA = filterBySearchTerm(VANS_ARTICLES_DATA);
+
+  const routes = [
+    { path: "/nike", articles: filtered_NIKE_ARTICLES_DATA },
+    { path: "/adidas", articles: filtered_ADIDAS_ARTICLES_DATA },
+    { path: "/reebok", articles: filtered_REEBOK_ARTICLES_DATA },
+    { path: "/puma", articles: filtered_PUMA_ARTICLES_DATA },
+    { path: "/vans", articles: filtered_VANS_ARTICLES_DATA },
+    { path: "/", articles: filtered_NIKE_ARTICLES_DATA },
+  ];
 
   return (
     <div className={classes.mainContent}>
       <Brands />
       <Routes>
-        <Route
-          path="/nike"
-          element={<Articles articles={filtered_NIKE_ARTICLES_DATA} />}
-        />
-        <Route
-          path="/adidas"
-          element={<Articles articles={ADIDAS_ARTICLES_DATA} />}
-        />
-        <Route
-          path="/reebok"
-          element={<Articles articles={REEBOK_ARTICLES_DATA} />}
-        />
-        <Route
-          path="/puma"
-          element={<Articles articles={PUMA_ARTICLES_DATA} />}
-        />
-        <Route
-          path="/vans"
-          element={<Articles articles={VANS_ARTICLES_DATA} />}
-        />
-        <Route
-          path="/"
-          element={<Articles articles={filtered_NIKE_ARTICLES_DATA} />}
-        />
+        {routes.map(({ path, articles }) => (
+          <Route path={path} element={<Articles articles={articles} />} />
+        ))}
       </Routes>
     </div>
   );
