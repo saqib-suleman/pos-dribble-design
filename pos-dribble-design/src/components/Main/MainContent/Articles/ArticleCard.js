aaa05bc5296631700e19241171b8dc21cffe68de
Saqib Suleman <saqibs3291@gmail.com>
2023-02-08 11:29:05 +0500

    improved styling

--- pos-dribble-design/src/components/Main/MainContent/Articles/ArticleCard.js
+++ pos-dribble-design/src/components/Main/MainContent/Articles/ArticleCard.js
@@ -8,26 +8,36 @@ function ArticleCard({ article }) {
     setMouseEnter(true);
   };
 
-  // const mouseLeaveHandler = () => {
-  //   setMouseEnter(false);
-  // };
-  //      onMouseLeave={mouseLeaveHandler}
+  const mouseLeaveHandler = () => {
+    setMouseEnter(false);
+  };
 
   return (
-    <>
-      <div className={classes.Card} onMouseEnter={mouseEnterHandler}>
-        <article className={classes.upperBody}>
-          <img src={article.imgUrl} />
-        </article>
-        <span className={classes.lowerBody}>{article.name}</span>
-        {mouseEnter ? (
-          <div className={classes.on_enter}>
-            <span>{article.price}</span>
-            <button>Add to cart</button>
-          </div>
-        ) : null}
-      </div>
-    </>
+    <div
+      className={classes.Card}
+      onMouseEnter={mouseEnterHandler}
+      onMouseLeave={mouseLeaveHandler}
+    >
+      <article className={classes.upperBody}>
+        <img src={article.imgUrl} />
+      </article>
+      <span className={classes.lowerBody}>{article.name}</span>
+      {mouseEnter && (
+        <div className={classes.on_enter}>
+          {article.inStock === 0 ? (
+            <div className={classes.on_enter_body}>
+              <p>Out of stock</p>
+              <button className={classes.disabled_button}>Add to cart</button>
+            </div>
+          ) : (
+            <div className={classes.on_enter_body}>
+              <span>{article.price}</span>
+              <button>Add to cart</button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
   );
 }
 
