51993e08a520b40b337e9326a57de7cdd7f25609
Saqib Suleman <saqibs3291@gmail.com>
2023-02-20 19:11:11 +0500

    added clear cart logic

--- pos-dribble-design/src/components/UI/Modal.js
+++ pos-dribble-design/src/components/UI/Modal.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import classes from "./Modal.module.css";
 
 function Modal({ text, setShowModal }) {
+  const cartCtx = useContext(CartContext);
+
+  const onClickHandler = () => {
+    cartCtx.clearCart();
+    setShowModal(false);
+  };
+
   return (
-    <div onClick={() => setShowModal(false)} className={classes.Modal}>
+    <div onClick={onClickHandler} className={classes.Modal}>
       <span>{text}</span>
     </div>
   );
